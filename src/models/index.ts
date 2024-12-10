049431d9eee9f88fc4c14430369b77bf96d3945b
Juan <juanmarcosmendozapacheco@gmail.com>
2024-12-10 00:02:50 -0400

    Modelo de las tablas Invetory and HistoryInventory

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 export * from './categories.model';
-export * from './products.model';
+export * from './inventory.model';
 export * from './role.model';
 export * from './services.model';
 export * from './user.model';
+export * from './inventory-history.model'
