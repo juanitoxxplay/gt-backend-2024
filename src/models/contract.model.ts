7306abeb526abcc4a9c14fb69e9a925c8f7d1d5b
themanuacs <colmenares.manuel.arturo@gmail.com>
2024-12-15 17:41:25 -0400

    Todas las capas de RRHH hechas

--- src/models/contract.model.ts
+++ src/models/contract.model.ts
@@ -1,73 +1,45 @@
 import { DataTypes } from "sequelize";
+
 const ContractModel = {
-    id_contract: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    id_employee: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    start_date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-            isDate: true,
-            notEmpty: true,
-        },
-    },
-    salary:{
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-    },
-    position_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: "position",
-            key: "id_position",
-        },
-    },
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull:false,
+  },
+  salary: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  hours: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  days: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  id_empleoyee: {
+    type: DataTypes.INTEGER,
+  },
+  id_charge: {
+    type: DataTypes.INTEGER,
+  },
+  
+  id_departament: {
+    type: DataTypes.INTEGER,
+  },
+};
 
-    status: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
-    },
-
-    deparment_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: "department",
-            key: "id_department",
-        },
-    },
-
-    working_hours: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-            isNumeric: true,
-            notEmpty: true,
-        },
-    },
-    numbers_work_days: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-            isNumeric: true,
-            notEmpty: true,
-        },
-    },
-    final_date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-            isDate: true,
-            notEmpty: true,
-        },
-    },
-
-      };
-      export {ContractModel};
+export { ContractModel };
