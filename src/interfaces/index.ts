24839d78fc87e34ee9f057c8afeba6d0393242e1
Yortrono <yorbelcarrero@gmail.com>
2025-01-20 22:08:14 -0400

    Interface and it index

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -20,3 +20,4 @@ export * from "./supplier.interface";
 export * from "./tourist-package.interface";
 export * from "./unitMeasurement.interface";
 export * from "./user.interface";
+export * from "./booking-transport.interface";
