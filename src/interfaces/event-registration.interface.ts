01547245b6c7690a9a373e5bf7739eeff2a17ee2
Juan <juandguarecuco@gmail.com>
2025-01-22 23:41:19 -0400

    commit despues de fusion con event-registration

--- src/interfaces/event-registration.interface.ts
+++ src/interfaces/event-registration.interface.ts
@@ -1,11 +1,11 @@
-import { ClientInterface } from "./client.interface";
+//import { ClientInterface } from "./client.interface";
 import { EventInterface } from "./event.interface";
 export interface EventRegistrationInterface{
     id?:number;
     eventId:number;
     event: EventInterface;
     clienId:number;
-    client: ClientInterface;
+  //  client: ClientInterface;
     registrationDate:Date;
     status:number;
     unitPrice:number;
